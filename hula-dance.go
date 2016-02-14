abc6f26ae06a9b498bc21c78d908ebeeda9c1f04
Nex Zhu <nexzhu@gmail.com>
2016-02-14 09:49:03 +0800

    make it outmost color sensitive

---
+++ hula-dance.go
@@ -0,0 +1,166 @@
+package main
+
+type solution struct {
+    score       int
+    single      bool
+    left, right int
+    kill, last  []int
+}
+
+type palindrome struct {
+    flag   bool
+    length int
+    kill   []int
+    mid    int
+}
+
+func solveHulaDance(monsters []int, nColor int) [][]solution {
+    p := findPalindromes(monsters, nColor)
+
+    length := len(monsters)
+    s := make([][][][]solution, length)
+    for i := 0; i < length; i++ {
+        s[i] = make([][][]solution, length)
+        for j := 0; j < length; j++ {
+            s[i][j] = make([][]solution, nColor)
+            for c := 1; c < nColor; c++ {
+                s[i][j][c] = make([]solution, nColor)
+                if (p[i][j][c].flag) {
+                    s[i][j][c][c] = solution{p[i][j][c].length - 1, true, c, c, p[i][j][c].kill, []int{p[i][j][c].mid}}
+                }
+            }
+        }
+    }
+
+    for i := 4; i <= length; i++ {
+        for l := 0; l <= length - i; l++ {
+            r := l + i - 1
+            for m := l + 3; m < r - 3; m++ {
+                for llc := 1; llc < nColor; llc++ {
+                    for lrc := 1; lrc < nColor; lrc++ {
+                        for rc := 1; rc < nColor; rc++ {
+                            if (rc == lrc) {
+                                continue
+                            }
+                            curr := &s[l][r][llc][rc]
+                            ls := s[l][m][llc][lrc]
+                            rs := s[m + 1][r][rc][rc]
+                            if !(ls.score > 0 && rs.single && rs.score > 0 && ls.score + rs.score > curr.score) {
+                                continue
+                            }
+
+                            newKill, newLast := []int{}, []int{}
+                            newKill, newLast = append(newKill, ls.kill...), append(newLast, ls.last...)
+                            newKill, newLast = append(newKill, rs.kill...), append(newLast, rs.last...)
+                            *curr = solution{ls.score + rs.score, false, llc, rc, newKill, newLast}
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return s[0][length - 1]
+}
+
+func findPalindromes(monsters []int, nColor int) [][][]palindrome {
+    length := len(monsters)
+    p := make([][][]palindrome, length)
+    for i := 0; i < length; i++ {
+        p[i] = make([][]palindrome, length)
+        for j := 0; j < length; j++ {
+            p[i][j] = make([]palindrome, nColor)
+        }
+        p[i][i][monsters[i]] = palindrome{true, 1, []int{}, i}
+    }
+
+    for i := 4; i <= length; i++ {
+        for l := 0; l <= length - i; l++ {
+            r := l + i - 1
+            for ml := l + 1; ml < r; ml++ {
+                for mr := ml; mr < r; mr++ {
+                    midPalindromes := p[ml][mr]
+
+                    for c := 1; c < nColor; c++ {
+                        curr := &p[l][r][c]
+
+                        midPalindrome := &midPalindromes[c]
+                        if (midPalindrome.flag && (!curr.flag || curr.length < midPalindrome.length)) {
+                            newKill := appendKill(l, r, ml, mr, -1, -1, -1, midPalindrome.kill)
+                            *curr = palindrome{true, midPalindrome.length, newKill, midPalindrome.mid}
+                        }
+
+                        mcMax := -1
+                        max := 0
+                        for mc := 0; mc < nColor; mc++ {
+                            if mc != c && midPalindromes[mc].flag && midPalindromes[mc].length > max {
+                                mcMax = mc
+                                max = midPalindromes[mc].length
+                            }
+                        }
+                        if mcMax == -1 || (curr.flag && max < curr.length) {
+                            continue
+                        }
+
+                        midPalindrome = &midPalindromes[mcMax]
+                        enough, c1, c2, c3 := countWrap(monsters, c, l, ml - 1, mr + 1, r)
+                        if (!enough) {
+                            continue
+                        }
+                        newKill := appendKill(l, r, ml, mr, c1, c2, c3, midPalindrome.kill)
+                        *curr = palindrome{true, max + 1, newKill, midPalindrome.mid}
+                    }
+                }
+            }
+        }
+    }
+
+    return p
+}
+
+func countWrap(monsters []int, c, ll, lr, rl, rr int) (bool, int, int, int) {
+    lc, l1, l2 := count(monsters[ll:lr + 1], c)
+    l1 += ll
+    l2 += ll
+    rc, r1, r2 := count(monsters[rl:rr + 1], c)
+    r1 += rl
+    r2 += rl
+    if lc > 1 && rc > 0 {
+        return true, l1, l2, r1
+    } else if lc > 0 && rc > 1 {
+        return true, l1, r1, r2
+    } else {
+        return false, -1, -1, -1
+    }
+}
+
+func count(monsters []int, color int) (count, c1, c2 int) {
+    count, c1, c2 = 0, -1, -1
+    for i, c := range monsters {
+        if c == color {
+            count++
+            if count == 1 {
+                c1 = i
+            } else if count == 2 {
+                c2 = i
+            }
+        }
+    }
+    return count, c1, c2
+}
+
+func appendKill(l, r, ml, mr, c1, c2, c3 int, midKill []int) []int {
+    newKill := []int{}
+    for i := l; i < ml; i++ {
+        if i != c1 && i != c2 {
+            newKill = append(newKill, i)
+        }
+    }
+    newKill = append(newKill, midKill...)
+    for i := mr + 1; i <= r; i++ {
+        if i != c2 && i != c3 {
+            newKill = append(newKill, i)
+        }
+    }
+    return newKill
+}
--- test.go
+++ test.go
@@ -1,156 +1,113 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 //	"strings"
 //	"errors"
-	"errors"
-	"strings"
+    "errors"
+    "strings"
 )
 
+const nColor = 6
+
 func main() {
-	colorToInt := map[byte]int{'!': 0, 'r': 1, 'y': 2, 'g': 3, 'b': 4, 'p': 5}
-	intToColor := map[int]byte{}
-	for key, value := range colorToInt {
-		intToColor[value] = key
-	}
+    colorToInt := map[byte]int{'!': 0, 'r': 1, 'y': 2, 'g': 3, 'b': 4, 'p': 5}
+    intToColor := map[int]byte{}
+    for key, value := range colorToInt {
+        intToColor[value] = key
+    }
 
-	//	monsters := []int{2, 2, 1, 2}
-	//	monsters := []int{2, 2, 1, 3, 2}
-	//	monsters := []int{2, 2, 1, 2, 3}
-	//	monsters := []int{3, 2, 2, 1, 2, 3, 3}
-	//	monsters := []int{3, 4, 2, 2, 1, 2, 3, 3}
-	//	monsters := []int{3, 4, 2, 2, 1, 2, 3, 3, 2, 2, 3, 2}
-	//	monsters := []int{1, 2, 2, 4, 1, 3, 0, 0, 1, 0, 4, 1, 2, 4, 3, 4, 1, 0, 2, 1, 0, 1, 3, 3, 2}
-	// Real data 1
-	//	monsters := []int{1, 1, 2, 3, 1, 1, 2, 3, 3, 1, 1, 3, 1, 3, 4, 4, 2, 4, 3, 3, 3, 1, 4, 1, 3, 2, 4, 2, 4, 2, 1, 3, 2, 1, 5, 4, 2, 1, 5, 4, 4, 1, 4, 2, 1, 2, 2, 4, 2, 2, 1, 4, 5, 2, 2, 1, 5, 4, 4, 5}
-	// Real data 2
-	//	monsters := []int{2, 2, 5, 1, 2, 5, 4, 1, 4, 5, 1, 5, 5, 5, 4, 4, 1, 6, 1, 1, 2, 1, 2, 2, 2, 4, 1, 1, 2, 4, 2, 2, 6, 1, 4, 1, 3, 1, 5, 3, 4, 5, 6, 3, 4, 3, 3, 1, 4, 3, 5, 3, 5, 3, 3, 4, 4, 6, 5, 1, 5, 5, 4, 4, 4, 4, 3, 1, 3, 3, 5, 4, 3, 5, 5, 2, 5, 4, 3, 4, 2, 2, 5, 2, 3, 3, 3, 3, 3, 2, 5, 6, 5, 3, 4, 5, 5, 2, 2, 6, 2, 5, 3, 3, 2, 5, 5, 2, 3, 5, 2, 2, 2, 3, 2, 6, 2, 5, 2, 3, 1, 5, 3, 3, 6, 5, 5, 3, 3, 2, 1, 3, 2, 5, 1, 5, 1, 1, 4, 4, 1, 1, 1, 4}
+    //	monsters := []int{2, 2, 1, 2}
+    //	monsters := []int{2, 2, 1, 3, 2}
+    //	monsters := []int{2, 2, 1, 2, 3}
+    //	monsters := []int{3, 2, 2, 1, 2, 3, 3}
+    //	monsters := []int{3, 4, 2, 2, 1, 2, 3, 3}
+    //	monsters := []int{3, 4, 2, 2, 1, 2, 3, 3, 2, 2, 3, 2}
+    //	monsters := []int{1, 2, 2, 4, 1, 3, 0, 0, 1, 0, 4, 1, 2, 4, 3, 4, 1, 0, 2, 1, 0, 1, 3, 3, 2}
+    // Real data 1
+    //	monsters := []int{1, 1, 2, 3, 1, 1, 2, 3, 3, 1, 1, 3, 1, 3, 4, 4, 2, 4, 3, 3, 3, 1, 4, 1, 3, 2, 4, 2, 4, 2, 1, 3, 2, 1, 5, 4, 2, 1, 5, 4, 4, 1, 4, 2, 1, 2, 2, 4, 2, 2, 1, 4, 5, 2, 2, 1, 5, 4, 4, 5}
+    // Real data 2
+    //	monsters := []int{2, 2, 5, 1, 2, 5, 4, 1, 4, 5, 1, 5, 5, 5, 4, 4, 1, 6, 1, 1, 2, 1, 2, 2, 2, 4, 1, 1, 2, 4, 2, 2, 6, 1, 4, 1, 3, 1, 5, 3, 4, 5, 6, 3, 4, 3, 3, 1, 4, 3, 5, 3, 5, 3, 3, 4, 4, 6, 5, 1, 5, 5, 4, 4, 4, 4, 3, 1, 3, 3, 5, 4, 3, 5, 5, 2, 5, 4, 3, 4, 2, 2, 5, 2, 3, 3, 3, 3, 3, 2, 5, 6, 5, 3, 4, 5, 5, 2, 2, 6, 2, 5, 3, 3, 2, 5, 5, 2, 3, 5, 2, 2, 2, 3, 2, 6, 2, 5, 2, 3, 1, 5, 3, 3, 6, 5, 5, 3, 3, 2, 1, 3, 2, 5, 1, 5, 1, 1, 4, 4, 1, 1, 1, 4}
 
-	for {
-		fmt.Print("Last right color: ")
-		var lastRightStr string
-		for err := errors.New(""); err != nil; {
-			_, err = fmt.Scanln(&lastRightStr)
-		}
-		lastRight := colorToInt[lastRightStr[0]]
+    for {
+        fmt.Print("Last right color: ")
+        var lastRightStr string
+        for err := errors.New(""); err != nil; {
+            _, err = fmt.Scanln(&lastRightStr)
+        }
+        lastRight := colorToInt[lastRightStr[0]]
 
-		fmt.Print("Start index: ")
-		var start int
-		for err := errors.New(""); err != nil; {
-			_, err = fmt.Scanln(&start)
-		}
+        fmt.Print("Start index: ")
+        var start int
+        for err := errors.New(""); err != nil; {
+            _, err = fmt.Scanln(&start)
+        }
 
-		fmt.Println("Monsters: ")
-		var monstersStr string
-		for err := errors.New(""); err != nil; {
-			_, err = fmt.Scanln(&monstersStr)
-		}
+        fmt.Println("Monsters: ")
+        var monstersStr string
+        for err := errors.New(""); err != nil; {
+            _, err = fmt.Scanln(&monstersStr)
+        }
 
-		monstersColor := strings.Split(monstersStr, "")
-		monsters := []int{}
-		for _, value := range monstersColor {
-			monsters = append(monsters, colorToInt[value[0]])
-		}
+        monstersColor := strings.Split(monstersStr, "")
+        monsters := []int{}
+        for _, value := range monstersColor {
+            monsters = append(monsters, colorToInt[value[0]])
+        }
 
-		//lastRight := 5
-		//start := 1
-		//nColor := 5
-		//nMonster := 250
-		//monsters := Generate(nColor, nMonster)
+        //lastRight := 0
+        //start := 1
+        //nMonster := 250
+        //monsters := Generate(nColor, nMonster)
 
-		fmt.Println("")
-		//	fmt.Println(monsters)
-		fragment := dp(monsters, 6)
-		fmt.Println("score:", fragment.score)
-		fmt.Println("")
+        fmt.Println()
+        //	fmt.Println(monsters)
+        solutions := solveHulaDance(monsters, nColor)
 
-		minKill, minLast := 52, 52
-		minKillSolution, minLastSolution := -1, -1
+        max := -1
+        maxSolutions := []solution{}
+        //solution := solution{}
+        for lc := 1; lc < nColor; lc++ {
+            if (lc == lastRight) {
+                continue
+            }
 
-		for key, solution := range fragment.solution {
-			if (solution.left != lastRight && len(solution.kill) < minKill) {
-				minKill = len(solution.kill)
-				minKillSolution = key
-			}
-			if (solution.left != lastRight && len(solution.last) < minLast) {
-				minLast = len(solution.last)
-				minLastSolution = key
-			}
-		}
+            for rc := 1; rc < nColor; rc++ {
+                if solutions[lc][rc].score > max {
+                    maxSolutions = []solution{}
+                    max = solutions[lc][rc].score
+                }
+                if solutions[lc][rc].score >= max {
+                    maxSolutions = append(maxSolutions, solutions[lc][rc])
+                }
+            }
+        }
 
-		if (minKillSolution != -1) {
-			solution := fragment.solution[minKillSolution]
-			result := kill(monsters, solution.kill)
-			resultColor := []byte{}
-			for key, value := range solution.kill {
-				solution.kill[key] = start + value
-			}
-			for key, value := range solution.last {
-				solution.last[key] = start + value
-			}
-			for _, value := range result {
-				resultColor = append(resultColor, intToColor[value])
-			}
-			resultStr := string(resultColor)
+        for k, s := range maxSolutions {
+            fmt.Println("solution", k + 1)
+            result := kill(monsters, s.kill)
+            resultColor := []byte{}
+            for key, value := range s.kill {
+                s.kill[key] = start + value
+            }
+            for key, value := range s.last {
+                s.last[key] = start + value
+            }
+            for _, value := range result {
+                resultColor = append(resultColor, intToColor[value])
+            }
+            resultStr := string(resultColor)
 
-			fmt.Println("Min kill solution:")
-			//			fmt.Println("left:", solution.left)
-			//			fmt.Println("right:", solution.right)
-			fmt.Println("kill:", solution.kill)
-			fmt.Println("result:", resultStr)
-			fmt.Println("last:", solution.last)
-			fmt.Println()
-		}
+            fmt.Println("score:", max)
+            //fmt.Println("left:", s.left)
+            //fmt.Println("right:", s.right)
+            fmt.Println("kill:", s.kill)
+            fmt.Println("result:", resultStr)
+            fmt.Println("last:", s.last)
+            fmt.Println("")
+        }
 
-		if (minLastSolution != -1 && minLastSolution != minKillSolution) {
-			solution := fragment.solution[minLastSolution]
-			result := kill(monsters, solution.kill)
-			resultColor := []byte{}
-			for key, value := range solution.kill {
-				solution.kill[key] = start + value
-			}
-			for key, value := range solution.last {
-				solution.last[key] = start + value
-			}
-			for _, value := range result {
-				resultColor = append(resultColor, intToColor[value])
-			}
-			resultStr := string(resultColor)
-
-			fmt.Println("Min last solution")
-			//			fmt.Println("left:", solution.left)
-			//			fmt.Println("right:", solution.right)
-			fmt.Println("kill:", solution.kill)
-			fmt.Println("result:", resultStr)
-			fmt.Println("last:", solution.last)
-			fmt.Println()
-		}
-
-		//for key, solution := range fragment.solution {
-		//	result := kill(monsters, solution.kill)
-		//	resultColor := []byte{}
-		//	for key, value := range solution.kill {
-		//		solution.kill[key] = start + value
-		//	}
-		//	for key, value := range solution.last {
-		//		solution.last[key] = start + value
-		//	}
-		//	for _, value := range result {
-		//		resultColor = append(resultColor, intToColor[value])
-		//	}
-		//	resultStr := string(resultColor)
-		//
-		//	fmt.Println("Solution", key)
-		//	//fmt.Println("left:", solution.left)
-		//	//fmt.Println("right:", solution.right)
-		//	fmt.Println("kill:", solution.kill)
-		//	fmt.Println("result:", resultStr)
-		//	fmt.Println("last:", solution.last)
-		//	fmt.Println()
-		//}
-
-		fmt.Println("************************************")
-		fmt.Println("")
-	}
+        fmt.Println("************************************")
+        fmt.Println("")
+    }
 }
